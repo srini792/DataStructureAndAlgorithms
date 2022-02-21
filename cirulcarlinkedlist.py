336c25b1112361956e1c157549af794fbd1e3c8a
asrinivas7 <srinivasa@pagemajik.ai>
2022-02-21 22:28:01 +0530

    deletion is pending

--- cirulcarlinkedlist.py
+++ cirulcarlinkedlist.py
@@ -16,10 +16,12 @@ class circularlinkedlist():
         if n.nref is self.head:
             print(str(n.data)+" --->",end="")
             return
-        while n.nref is not self.head:
+        else:
+            while n.nref is not self.head:
+                print(str(n.data)+" --->",end="")
+                n = n.nref
             print(str(n.data)+" --->",end="")
-            n = n.nref
-        print()
+            print()
 
 
     def insert_begin(self,data):
@@ -38,9 +40,22 @@ class circularlinkedlist():
             n = n.nref
         new_node.nref = n.nref
         n.nref = new_node
-    # def insert_middle(self,data):
-
+    
+    def insert_middle(self,data,x):
+        new_node = Node(data)
+        n = self.head
+        while n.nref is not self.head:
+            if n.data == x:
+                break
+            n = n.nref
+        if n.data == x:
+            new_node.nref = n.nref
+            n.nref = new_node
+        else:
+            print("unable to insert, element not present in list...!")
 cl = circularlinkedlist()
-cl.insert_begin(10)
+# cl.insert_begin(10)
 cl.insert_end(11)
+cl.insert_middle(10.5,11)
+cl.insert_middle(12,11)
 cl.print()
