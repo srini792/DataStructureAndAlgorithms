1bfc1785995201bbd822603cbc3bbf856d744951
asrinivas7 <srinivasa@pagemajik.ai>
2022-03-10 22:53:10 +0530

    upto insertion

--- BinarySearchtree.py
+++ BinarySearchtree.py
@@ -3,6 +3,22 @@ class BinarySearchTree():
         self.key = key
         self.lchild = None
         self.rchild = None
+    def insert(self,data):
+        if self.key is None:
+            self.key = data
+            return
+        if self.key == data:
+            return
+        if self.key > data:
+            if self.lchild:
+                self.lchild.insert(data)
+            else:
+                self.lchild = BinarySearchTree(data)
+        else:
+            if self.rchild:
+                self.rchild.insert(data)
+            else:
+                self.rchild = BinarySearchTree(data)
 
 root = BinarySearchTree(10)
 root.lchild = BinarySearchTree(9)
