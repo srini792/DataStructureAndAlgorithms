f38b2bc150f6a81b104312e06c3a43a8004a4370
asrinivas7 <srinivasa@pagemajik.ai>
2022-03-15 23:14:47 +0530

    search completed

--- BinarySearchtree.py
+++ BinarySearchtree.py
@@ -3,6 +3,8 @@ class BinarySearchTree():
         self.key = key
         self.lchild = None
         self.rchild = None
+
+        
     def insert(self,data):
         if self.key is None:
             self.key = data
@@ -19,14 +21,36 @@ class BinarySearchTree():
                 self.rchild.insert(data)
             else:
                 self.rchild = BinarySearchTree(data)
-    def search(self,data):
-        
 
-root = BinarySearchTree(10)
-root.lchild = BinarySearchTree(9)
-print(root.key)
-print(root.lchild)
-print(root.rchild)
-print(root.lchild.key)
-print(root.lchild.lchild)
-print(root.lchild.rchild)
+    
+    def search(self,data):
+        if self.key == data:
+            print("Node Found...!")
+            return
+        if self.key > data:
+            if self.lchild:
+                self.lchild.search(data)
+            else:
+                print("Node Not Found..!")
+        else:
+            if self.rchild:
+                self.rchild.search(data)
+            else:
+                print("Node Not Found...!")
+
+
+root = BinarySearchTree(100)
+
+list1 = [10,20,30,300,200,150]
+
+for i in list1:
+    root.insert(i)
+# root.lchild = BinarySearchTree(9)
+# print(root.key)
+# print(root.lchild)
+# print(root.rchild)
+# print(root.lchild.key)
+# print(root.lchild.lchild)
+# print(root.lchild.rchild)
+
+root.search(50)
