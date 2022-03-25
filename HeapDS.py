fb7195a533e6beccb02c21b5605063e33a91ad19
asrinivas7 <srinivasa@pagemajik.ai>
2022-03-25 23:40:39 +0530

    binary heap tree

---
+++ HeapDS.py
@@ -0,0 +1,8 @@
+# min and max Binary heap tree
+# 1) min binary heap tree
+# 2) max binary heap tree
+
+# purpose 
+# 1) To implement priority queue
+# 2) In heap sort algorithm
+# 3) To find Kth largest or smallest element in list of number 
