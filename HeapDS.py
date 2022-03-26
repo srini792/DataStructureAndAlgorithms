f6dc98dd638ed62233b386dae79255c276bede7e
asrinivas7 <srinivasa@pagemajik.ai>
2022-03-27 00:18:10 +0530

    using heapq module

--- HeapDS.py
+++ HeapDS.py
@@ -6,3 +6,15 @@
 # 1) To implement priority queue
 # 2) In heap sort algorithm
 # 3) To find Kth largest or smallest element in list of number 
+
+# using heapq module
+import heapq
+
+heap = []
+
+heapq.heappush(heap,100)
+heapq.heappush(heap,50)
+heapq.heappush(heap,60)
+heapq.heappush(heap,55)
+heapq.heappush(heap,90)
+print(heap)
