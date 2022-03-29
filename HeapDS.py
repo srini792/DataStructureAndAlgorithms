697512bbe7b834c1afe38dc2dbe83ad81d8debec
asrinivas7 <srinivasa@pagemajik.ai>
2022-03-30 00:55:12 +0530

    heapq module basic  functions

--- HeapDS.py
+++ HeapDS.py
@@ -18,3 +18,36 @@ heapq.heappush(heap,60)
 heapq.heappush(heap,55)
 heapq.heappush(heap,90)
 print(heap)
+
+
+heap1 = [100,99,98,97]
+
+#heappop
+heapq.heapify(heap1)
+heapq.heappop(heap1)
+print(heap1)
+
+#pusppop
+heap2 = [100,98,99,101]
+heapq.heapify(heap2)
+print(heapq.heappushpop(heap2,102))
+print(heap2)
+
+#heapq.nsmallest
+heap3 = [5,4,3,2,1]
+heapq.heapify(heap3)
+print(heapq.nsmallest(2,heap3))
+
+#heapq.nlargest
+heap4 = [5,4,3,2,1]
+heapq.heapify(heap4)
+print(heapq.nlargest(2,heap4))
+
+
+#priority queue
+pq = [(100,"srinivas"),(1,"tvl")]
+heapq.heapify(pq)
+print(pq)
+print(heapq.heappop(pq))
+print(pq)
+
